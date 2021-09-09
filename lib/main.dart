5c77f5ccbe2b4a5ce8a46fef9649121a8cff6311
bdeguigne <brice.deguigne@epitech.eu>
2021-09-09 18:26:50 +0200

    first screen app workshop

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bpl/first_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'first_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: "Montserrat",
+        primarySwatch: Colors.blue,
+      ),
+      home: const FirstScreen(),
+    );
+  }
+}
